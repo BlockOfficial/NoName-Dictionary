bf7f2ac708aac1b1cc66686bc90839360bc052bd
BlockOfficial <86261003+BlockOfficial@users.noreply.github.com>
2021-06-21 19:36:52 +0100

    NoNameDictionary.py
    This is the code for the NoName Dictionary.

---
+++ NoNameDicitionary.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root=Tk()
+root.title("NoName Dictionary")
+root.geometry("400x250")
+title = Label(root, text="NoName Dictionary", font=("Poppins", 30)).pack(ipady=10)
+Label(root, text="This app is currently in development.", font=("Poppins", 15)).pack(ipady=1)
+
+
+root.mainloop()
